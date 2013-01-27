d9d3e5799ee052577ea6052afb235599c63398ae
Anthony Scopatz <scopatz@gmail.com>
2013-01-26 18:51:01 -0600

    some project level stuff.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+
+setup(name="fist",
+	version='0.1-dev',
+	description='Fi',
+	author='John Bachan, Anthony Scopatz',
+	author_email='john.bachan@gmail.com, scopatz@gmail.com',
+	url='https://github.com/scopatz/fist',
+	packages=['fist',],
+	package_dir={'fist': 'fist'}, 
+    scripts=['scripts/fist'],
+	)
+
