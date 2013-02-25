9443152bd582db58ffd514e595cba5f6c9131018
Anthony Scopatz <scopatz@gmail.com>
2013-02-24 19:45:38 -0600

    mostly working version.

--- docs/_static/fief-dungeon.js
+++ docs/_static/fief-dungeon.js
@@ -1,23 +1,23 @@
 /*global ut */
 var term, eng; // Can't be initialized yet because DOM is not ready
-var pl = { x: 3, y: 2 }; // Player position
+var xsize = 21;
+var ysize = 21;
+var pl = { x: 2*xsize, y: ysize+2 }; // Player position
 var updateFOV; // For some of the examples
-var map = [
-"#######################################",
-"#.....................................#",
-"#.....................................#",
-"#....####.....##................####..#",
-"#...##........##...............##.....#",
-"#...##.........................##.....#",
-"#..#####....####......#####...#####...#",
-"#...##........##.....##   ##...##.....#",
-"#...##........##.....#######...##.....#",
-"#...##........##.....##........##.....#",
-"#...##......######....#####....##.....#",
-"#...#..........................#......#",
-"#.....................................#",
-"#.....................................#",
-"#######################################",
+var fiefmap = [
+".....................................",
+".....................................",
+"....####.....##................####..",
+"...##........##...............##.....",
+"...##.........................##.....",
+"..#####....####......#####...#####...",
+"...##........##.....##   ##...##.....",
+"...##........##.....#######...##.....",
+"...##........##.....##........##.....",
+"...##......######....#####....##.....",
+"...#..........................#......",
+".....................................",
+".....................................",
 ];
 
 // The tile palette is precomputed in order to not have to create
@@ -59,15 +59,103 @@ function onKeyDown(k) {
 	tick();
 }
 
+function mazegen(x,y) {
+    var n=x*y-1;
+    if (n<0) {alert("illegal maze dimensions");return;}
+    var horiz=[]; for (var j= 0; j<x+1; j++) horiz[j]= [];
+    var verti=[]; for (var j= 0; j<y+1; j++) verti[j]= [];
+    var here= [Math.floor(Math.random()*x), Math.floor(Math.random()*y)];
+    var path= [here];
+    var unvisited= [];
+    for (var j= 0; j<x+2; j++) {
+        unvisited[j]= [];
+        for (var k= 0; k<y+1; k++)
+            unvisited[j].push(j>0 && j<x+1 && k>0 && (j != here[0]+1 || k != here[1]+1));
+    }
+    var fmxlen= fiefmap[0].length;
+    var fmylen= fiefmap.length;
+    var fmxlbound= Math.floor((x - fmxlen) / 2);
+    var fmxubound= fmxlbound + fmxlen;
+    var fmylbound= Math.floor((y - fmylen) / 2);
+    var fmyubound= fmylbound + fmylen;
+    while (0<n) {
+        var potential= [[here[0]+1, here[1]], [here[0],here[1]+1],
+            [here[0]-1, here[1]], [here[0],here[1]-1]];
+        var neighbors= [];
+        for (var j= 0; j < 4; j++)
+            if (unvisited[potential[j][0]+1][potential[j][1]+1])
+                neighbors.push(potential[j]);
+        if (neighbors.length) {
+            n= n-1;
+            next= neighbors[Math.floor(Math.random()*neighbors.length)];
+            unvisited[next[0]+1][next[1]+1]= false;
+            if (next[0] == here[0])
+                horiz[next[0]][(next[1]+here[1]-1)/2]= true;
+            else 
+                verti[(next[0]+here[0]-1)/2][next[1]]= true;
+            path.push(here= next);
+        } else 
+            here= path.pop();
+    }
+    return ({x: x, y: y, horiz: horiz, verti: verti});
+}
+ 
+function mazedisplay(m) {
+    var fmxlen= fiefmap[0].length;
+    var fmylen= fiefmap.length;
+    var fmxlbound= Math.floor(2*m.x - fmxlen/2);
+    var fmxubound= fmxlbound + fmxlen;
+    var fmylbound= Math.floor((m.y + fmylen) / 2);
+    var fmyubound= fmylbound + fmylen;
+    var text= [];
+    for (var j= 0; j<m.x*2+1; j++) {
+        var line= [];
+        if (0 == j%2)
+            for (var k=0; k<m.y*4+1; k++)
+                if (0 == k%4) 
+                    line[k]= '#';
+                else
+                    if (j>0 && m.verti[j/2-1][Math.floor(k/4)])
+                        line[k]= '.';
+                    else
+                        line[k]= '#';
+        else
+            for (var k=0; k<m.y*4+1; k++)
+                if (0 == k%4)
+                    if (k>0 && m.horiz[(j-1)/2][k/4-1])
+                        line[k]= '.';
+                    else
+                        line[k]= '#';
+                else
+                    line[k]= '.';
+        if (0 == j) line[1]= line[2]= line[3]= '.';
+        if (m.x*2-1 == j) line[4*m.y]= '.';
+        var l = line.join('');
+        console.log(j);
+        if (fmylbound <= j && j < fmyubound){
+            console.log(l.length);
+            var ll = l.slice(0, fmxlbound);
+            var lu = l.slice(fmxubound, 4*m.x+1);
+            l = ll + fiefmap[j-fmylbound] + lu;
+            console.log(l.length);
+        }
+        text.push(l);
+    }
+    return text;
+}
+
+var map = mazedisplay(mazegen(xsize, ysize))
+
 // Initialize stuff
 function initFiefDungeon() {
 	window.setInterval(tick, 50); // Animation
 	// Initialize Viewport, i.e. the place where the characters are displayed
-	term = new ut.Viewport(document.getElementById("game"), 61, 12);
+	term = new ut.Viewport(document.getElementById("game"), 61, 15);
 	// Initialize Engine, i.e. the Tile manager
 	eng = new ut.Engine(term, getDungeonTile, map[0].length, map.length);
 	// Initialize input
 	ut.initInput(onKeyDown);
 }
+
 
 
