614521419b3623714017336bf9c4420808e1ac20
Anthony Scopatz <scopatz@gmail.com>
2013-04-05 16:35:40 -0500

    updated select command to work

--- fief/cli/select.py
+++ fief/cli/select.py
@@ -7,9 +7,9 @@ USAGE = ("Adds interfaces to the current fief selection.\n\n"
 
 def main(ns, rc):
     """Adds interfaces to a fief selection."""
-    selection = _magic.env_selection(rc)
+    selection = _magic.env_selection()
     selection |= set(ns.ifcs)
-    env = {'FIEF_SELECTION': ' '.join(selection)}
+    env = {'FIEF_SELECTION': ' '.join(sorted(selection))}
     if ns.verbose:
         sys.stderr.write("current interface selection: " + ' '.join(selection) + '\n')
     _magic.exportvars(env)
