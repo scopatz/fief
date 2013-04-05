6eac33a6a6aa6b24c4dc06c9e67392de9f120714
Anthony Scopatz <scopatz@gmail.com>
2013-04-05 17:37:53 -0500

    added verbosity to stash

--- fief/cli/stash.py
+++ fief/cli/stash.py
@@ -7,22 +7,26 @@ from .. import async
 
 import _magic
 
-def _checkrm(p):
+def _checkrm(p, verbose=False):
     if os.path.isdir(p):
         shutil.rmtree(p)
     elif os.path.isfile(p):
         os.remove(p)
+    if verbose:
+        sys.stderr.write('removed: {0}\n'.format(os.path.abspath(p)))
 
 def clean(ns, finst):
     if ns.clean == 'all':
-        _checkrm(finst._path_stash)
+        _checkrm(finst._path_stash, ns.verbose)
     elif ns.clean == 'fetch':
-        _checkrm(finst.procurer._stash)
+        _checkrm(finst.procurer._stash, ns.verbose)
     elif ns.clean == 'build':
-        _checkrm(finst.oven._path)
+        _checkrm(finst.oven._path, ns.verbose)
 
 def main(ns, rcpath):
     """Manipulates the current stash."""
     finst = async.run(fief.Fief.new_a(rcpath))
+    if ns.verbose:
+        sys.stderr.write('stash: {0}\n'.format(os.path.abspath(finst._path_stash)))
     if ns.clean is not None:
         clean(ns, finst)
